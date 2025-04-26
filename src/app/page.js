5b160791a70d2ea0cf67fecd029575336ba6f7a5
armaanpriyadarshan <armaan_priyadarshan@outlook.com>
2025-04-26 15:27:43 -0400

    main

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import { IBM_Plex_Mono } from "next/font/google";
+import { IBM_Plex_Mono, Roboto_Condensed } from "next/font/google";
 
 const ibmPlexMono = IBM_Plex_Mono({
   subsets: ["latin"],
@@ -6,14 +6,22 @@ const ibmPlexMono = IBM_Plex_Mono({
   display: "swap",
 });
 
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["200"], // Thin weight
+  display: "swap",
+});
+
 export default function Home() {
   return (
-    <div className="min-h-screen bg-cream">
-      <div className="flex flex-col items-center justify-center">
-        <p className="text-black text-lg y-100" style={{ fontFamily: ibmPlexMono.style.fontFamily }}>
-          todays prompt is...
+    <div className="min-h-screen">
+      <div className="flex flex-col items-center justify-center m-12">
+        <p className={`text-brown ${ibmPlexMono.className} text-2xl underline`}>
+          today&apos;s prompt is...
         </p>
-        <p className="text-darkGreen text-lg">prompt</p>
+        <p className={`text-green ${robotoCondensed.className} text-6xl uppercase mt-4`}>
+          * prompt *
+        </p>
       </div>
     </div>
   );
