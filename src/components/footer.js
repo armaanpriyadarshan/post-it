fa3dd0621d94f367654d4db659e6fd3536fe55a7
Rohan Sarikonda <rohan.sarikonda@gmail.com>
2025-04-26 16:52:13 -0400

    idek bro it just works

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,33 @@
-import { AiOutlineInfo, AiOutlineEdit } from "react-icons/ai";
+'use client';
+
+import { useState } from 'react';
+import { AiOutlineInfo } from "react-icons/ai";
+import { IBM_Plex_Mono } from 'next/font/google';
+
+const ibmPlexMono = IBM_Plex_Mono({
+    subsets: ['latin'],
+    weight: ['400'],
+    display: 'swap',
+});
 
 export default function Footer() {
+    const [showInfo, setShowInfo] = useState(false);
+
     return (
-        <footer className="fixed bottom-0 right-0 m-6 flex items-center space-x-4">
-            <div className="w-12 h-12 flex items-center justify-center rounded-full bg-[var(--cafeBrown)] text-[var(--cream)] hover:bg-[var(--darkGreen)] transition cursor-pointer">
-                <AiOutlineInfo size={24} />
-                <AiOutlineEdit size={24} />
+        <footer className="fixed bottom-6 right-6 flex items-center space-x-6">
+            <div className="relative">
+                {showInfo && (
+                    <div className="absolute bottom-16 right-0 mb-2 p-4 rounded-lg bg-[var(--cafeBrown)] text-[var(--cream)] text-sm shadow-lg animate-smooth-fade-in w-64">
+                        <div className={`relative, ${ibmPlexMono.className}`}>
+                            welcome to post.it! during your stay, you can peruse the stories left by others, or leave your own sticky note for the next visitor to find.
+                            <div className="absolute -bottom-1 right-3 w-3 h-3 bg-[var(--cafeBrown)] rotate-45 transform origin-top-left"></div>
+                        </div>
+                    </div>
+                )}
+
+                <div onClick={() => setShowInfo(!showInfo)} className="w-12 h-12 flex items-center justify-center rounded-full border-2 border-[var(--cafeBrown)] text-[var(--cafeBrown)] bg-transparent hover:bg-[var(--cafeBrown)] hover:text-[var(--cream)] transition-colors duration-300 cursor-pointer">
+                    <AiOutlineInfo size={24} />
+                </div>
             </div>
         </footer>
     );
