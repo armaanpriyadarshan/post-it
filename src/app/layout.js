e345e1343f18dc03201ff0a521f43d9d9269d26b
Rohan Sarikonda <rohan.sarikonda@gmail.com>
2025-04-27 01:50:36 -0400

    added logo to top left on both pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +21,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
+        suppressHydrationWarning={true}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className = "absolute top-0 left-0 z-10 m-4">
+          <Image
+            src = "/logo.png"
+            alt = "postit logo"
+            width = {50}
+            height = {50}
+          />
+        </div>
         {children}
       </body>
     </html>
